5507385ef0bd9a05b52ca680b74712e4518e006c
Maia Werth <maia.moo@me.com>
2024-03-08 01:52:29 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    const [firstDriver, secondDriver] = drivers;
+    return [firstDriver, secondDriver];
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    const [secondLastDriver, lastDriver] = drivers.slice(-2);
+    return [secondLastDriver, lastDriver];
+  }
+  
+  const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+  ];
+  
+  function createFareMultiplier(multiplier) {
+    return function (value) {
+      return value * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers(drivers, selectingFunction) {
+    return selectingFunction(drivers);
+  }
